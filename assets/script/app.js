d5fbdeb95c430a81b86f13c52d42fa01eb4c1e56
Myeongjeon Lee <userclassdev@outlook.com>
2024-03-28 09:33:12 -0500

    refactor app.js code such that postBtn event listener is a lot shorter. Extract logic inside postBtn event listener into named functions outside the event listener

--- assets/script/app.js
+++ assets/script/app.js
@@ -18,14 +18,14 @@ const postBtn = select('.post-btn');
 
 listen('click', uploadBtn, function(event) {
   event.preventDefault();
-  imageUpload.click(); //this opens the file picker dialogue so u can select an image
+  imageUpload.click(); //This opens the file picker dialogue so u can select an image
 });
 
 listen('change', imageUpload, function() {
-  // a 'change' event occurs when you select a file from the file picker dialog
+  // A 'change' event occurs when you select a file from the file picker dialog
   if (this.files && this.files[0]) { //checks if at least 1 file has been selected
-    // set its text to the file name
-    fileName.textContent = this.files[0].name;// sets the span next to uploadBtn to the file name
+    // Set its text to the file name
+    fileName.textContent = this.files[0].name;// Sets the span next to uploadBtn to the file name
   }
 });
 
@@ -49,50 +49,54 @@ function createPostCard(postContent, postImageSrc) {
   return newPostCard;
 }
 
+// Function to handle image upload
+function handleImageUpload() {
+  const reader = new FileReader();
+  // Starts the process of reading the first selected file and converting it into a data: URL. Once the reading process is complete, you can access the data: URL through reader.result or postImageSrc
+  reader.readAsDataURL(imageUpload.files[0]);
+  // The function that is assigned to reader.onloadend gets called AFTER the reading is done
+  reader.onloadend = function() {
+    const postImageSrc = reader.result; //Data url of the uploaded image
+    createAndInsertPost(textarea.value, postImageSrc);
+    clearInputs();
+  }
+}
+
+// Function to handle post with text but without image
+function handlePostWithoutImage() {
+  createAndInsertPost(textarea.value, null);
+  clearInputs();
+}
+
+// Function to create and insert a new post card
+function createAndInsertPost(content, imageSrc) {
+  const newPostCard = createPostCard(content, imageSrc);
+  // Inserts newPostCard right after postComposer
+  postComposer.insertAdjacentElement('afterend', newPostCard);
+}
+
+// Function to clear inputs
+function clearInputs() {
+  // Clear the textarea and image upload input
+  textarea.value = '';
+  imageUpload.value = '';
+  // Clear the file name
+  select('.file-name').innerText = '';
+}
+
 listen('click', postBtn, function(event) {
   event.preventDefault();
 
+  // If the user doesn't type anything AND doesn't select an image. don't post anything
   if (textarea.value.trim() === '' && !imageUpload.files[0]) {
-    // if the user doesn't type anything and doesn't select an image. don't post anything
     return;
   }
 
-  let postImageSrc = null;
-
-  if (imageUpload.files[0]) { // if an image is selected
-    const reader = new FileReader();
-    // starts the process of reading the first selected file and converting it into a data: URL. Once the reading process is complete, you can access the data: URL through reader.result. in other words, data URL gets assigned to postImageSrc
-    reader.readAsDataURL(imageUpload.files[0]);
-    // the function that is assigned to reader.onloadend gets called AFTER the reading is done
-    reader.onloadend = function() {
-      postImageSrc = reader.result; //data url of the uploaded image
-
-      const newPostCard = createPostCard(
-        textarea.value,
-        postImageSrc
-      );  
-      // Inserts newPostCard right after postComposer
-      postComposer.insertAdjacentElement('afterend', newPostCard); 
-
-      // Clear the textarea and image upload input
-      textarea.value = '';
-      imageUpload.value = '';
-    }
-  } else { // If an image is NOT selected
-    const newPostCard = createPostCard(
-      textarea.value,
-      postImageSrc
-    );  
-
-    // inserts the new post card into the DOM
-    postComposer.insertAdjacentElement('afterend', newPostCard);
-
-    // Clear the textarea
-    textarea.value = '';
+  if (imageUpload.files[0]) { // If an image is selected
+    handleImageUpload();
+  } else { // If an image is NOT selected. i.e., there's only text and no image.
+    handlePostWithoutImage();
   }
-
-  // Clear the file name
-  select('.file-name').innerText = '';
 });
 
 const johnDoe = new Subscriber(
